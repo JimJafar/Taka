88da4531c6c737e826f98b08c7d1f538e3714305
Jim Sangwine <github@sangwine.net>
2012-12-22 22:02:40 +0100

    JSHint cleanup - replaced tabs with spaces.

--- src/main/js/assets/Cache.js
+++ src/main/js/assets/Cache.js
@@ -6,19 +6,19 @@ var Taka = (Taka) ? Taka : {};
  * @author Jim Sangwine
  */
 Taka.assets.Cache = (function() {
-	/**
-	 * @name _sprites
-	 * @field _sprites A collection of all sprites so far loaded
-	 */
-	var _sprites = {};
+    /**
+     * @name _sprites
+     * @field _sprites A collection of all sprites so far loaded
+     */
+    var _sprites = {};
 
-	/**
-	 * @name _keys
-	 * @field _keys A collection of the keys for all currently cached sprites
-	 */
-	var _keys = [];
+    /**
+     * @name _keys
+     * @field _keys A collection of the keys for all currently cached sprites
+     */
+    var _keys = [];
 
-	return {
+    return {
         /**
          * @name spriteCount
          * @function spriteCount Returns the number of sprites in the cache
@@ -28,61 +28,61 @@ Taka.assets.Cache = (function() {
             return _keys.length;
         },
 
-		/**
-		 * @name addSprite
-		 * @function addSprite Adds a sprite to the cache
-		 * @param sprite
-		 * @param key
-		 */
-		addSprite: function(sprite, key) {
-			_sprites[key] = sprite;
-			_keys.push(key);
-		},
+        /**
+         * @name addSprite
+         * @function addSprite Adds a sprite to the cache
+         * @param sprite
+         * @param key
+         */
+        addSprite: function(sprite, key) {
+            _sprites[key] = sprite;
+            _keys.push(key);
+        },
 
-		/**
-		 * @name removeSprite
-		 * @function removeSprite Removes a sprite from the cache
-		 * @param key
-		 */
-		removeSprite: function(key) {
-			var index = _keys.indexOf(key);
-			if (index === -1) return;
+        /**
+         * @name removeSprite
+         * @function removeSprite Removes a sprite from the cache
+         * @param key
+         */
+        removeSprite: function(key) {
+            var index = _keys.indexOf(key);
+            if (index === -1) return;
 
-			_sprites[key] = null;
-			delete _sprites[key];
-			_keys.splice(index, 1);
-		},
+            _sprites[key] = null;
+            delete _sprites[key];
+            _keys.splice(index, 1);
+        },
 
-		/**
-		 * @name hasSprite
-		 * @function hasSprite Returns true if the cache contains a sprite matching the given key, otherwise false
-		 * @param key
-		 * @return bool
-		 */
-		hasSprite: function(key) {
-			return _sprites.hasOwnProperty(key);
-		},
+        /**
+         * @name hasSprite
+         * @function hasSprite Returns true if the cache contains a sprite matching the given key, otherwise false
+         * @param key
+         * @return bool
+         */
+        hasSprite: function(key) {
+            return _sprites.hasOwnProperty(key);
+        },
 
-		/**
-		 * @name fetchSprite
-		 * @function fetchSprite Fetches a sprite from the cache
-		 * @param key
-		 */
-		fetchSprite: function(key) {
-			if (!_sprites.hasOwnProperty(key)) return null;
-			return _sprites[key];
-		},
+        /**
+         * @name fetchSprite
+         * @function fetchSprite Fetches a sprite from the cache
+         * @param key
+         */
+        fetchSprite: function(key) {
+            if (!_sprites.hasOwnProperty(key)) return null;
+            return _sprites[key];
+        },
 
-		/**
-		 * @name clear
-		 * @function clear Clears the cache
-		 */
-		clear: function() {
-			for (var i = _keys.length - 1; i >= 0; i--) {
-				_sprites[_keys[i]] = null;
-			}
-			_sprites = {};
-			_keys = [];
-		}
-	};
+        /**
+         * @name clear
+         * @function clear Clears the cache
+         */
+        clear: function() {
+            for (var i = _keys.length - 1; i >= 0; i--) {
+                _sprites[_keys[i]] = null;
+            }
+            _sprites = {};
+            _keys = [];
+        }
+    };
 })();
--- src/main/js/core/Config.js
+++ src/main/js/core/Config.js
@@ -6,13 +6,13 @@ var Taka = (Taka) ? Taka : {};
  * @property fps The target frames per second for the game engine
  */
 Taka.core.Config = {
-	fps: 60,
-	buttons: {
-		up: 38,
-		down: 40,
-		left: 37,
-		right: 39,
-		fire: 32,
-		pause: 27
-	}
+    fps: 60,
+    buttons: {
+        up: 38,
+        down: 40,
+        left: 37,
+        right: 39,
+        fire: 32,
+        pause: 27
+    }
 };
