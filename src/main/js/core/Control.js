020d4e5ef70389c57d484f6f5f0c1455c0916674
Jim Sangwine <github@sangwine.net>
2013-01-03 01:11:29 +0100

    Added preventDefault and stopPropagation to keyboard event handling (to prevent the browser from scrolling in response to keystrokes).

--- src/main/js/core/Control.js
+++ src/main/js/core/Control.js
@@ -8,6 +8,54 @@ var TakaConfig = (TakaConfig) ? TakaConfig : {};
  */
 Taka.core.Control = (function() {
     "use strict";
+
+    /**
+     * A keyboard event
+     * @memberof Taka.core.Control
+     * @type Event
+     * @private
+     */
+    var _event;
+
+    /**
+     * The keycode associated with a keyboard event
+     * @memberof Taka.core.Control
+     * @type string
+     * @private
+     */
+    var _code;
+
+    /**
+     * Prevents event bubbling up and any possible default effect on the browser
+     * @memberof Taka.core.Control
+     * @param {Event} event
+     * @private
+     */
+    var _stopPropagation = function(event) {
+        if(event.stopPropagation) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    };
+
+    /**
+     * Updates the _event and _code variables
+     * @memberof Taka.core.Control
+     * @param {Event} e
+     * @private
+     */
+    var _getEventAndCode = function(e) {
+        if(window.event) {
+            _event = window.event;
+            _code = _event.keyCode;
+        } else {
+            _event = e;
+            _code = _event.which;
+        }
+    };
+
     /**
      * @lends Taka.core.Control
      */
@@ -17,25 +65,31 @@ Taka.core.Control = (function() {
          * @param {Event} e The onKeyDown event
          */
         keyDown: function(e) {
-            var code = (window.event) ? window.event.keyCode : e.which;
-            switch(code) {
+            _getEventAndCode(e);
+            switch(_code) {
                 case TakaConfig.buttons.up:
                     Taka.core.Engine.Player().moveUp = true;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.down:
                     Taka.core.Engine.Player().moveDown = true;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.left:
                     Taka.core.Engine.Player().moveLeft = true;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.right:
                     Taka.core.Engine.Player().moveRight = true;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.fire:
                     Taka.core.Engine.Player().fire = true;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.pause:
                     Taka.core.Engine.Pause();
+                    _stopPropagation(_event);
                     break;
             }
         },
@@ -45,22 +99,27 @@ Taka.core.Control = (function() {
          * @param {Event} e The onKeyUp event
          */
         keyUp: function(e) {
-            var code = (window.event) ? window.event.keyCode : e.which;
-            switch(code) {
+            _getEventAndCode(e);
+            switch(_code) {
                 case TakaConfig.buttons.up:
                     Taka.core.Engine.Player().moveUp = false;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.down:
                     Taka.core.Engine.Player().moveDown = false;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.left:
                     Taka.core.Engine.Player().moveLeft = false;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.right:
                     Taka.core.Engine.Player().moveRight = false;
+                    _stopPropagation(_event);
                     break;
                 case TakaConfig.buttons.fire:
                     Taka.core.Engine.Player().fire = false;
+                    _stopPropagation(_event);
                     break;
             }
         }
